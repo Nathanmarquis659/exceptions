1830d0c73b1271099cb661e5ee92c89f32ab86c1
Nathanmarquis659 <nathanmarquis659@u.boisestate.edu>
2024-09-20 19:06:37 -0600

    Working

--- FormatChecker.java
+++ FormatChecker.java
@@ -10,45 +10,94 @@ import java.util.*;
 public class FormatChecker {
 
     /**
-     * Parses through a given file to write its contents to a instance of ___, returns said array
+     1) open file
+     2) parse contents
+     3) check if fits criteria (rows/columns and also doubles ONLY)
      * 
      * @param filename is a String used to open a file to read
      * @return 2d int array for obj read from a file
      * @throws FileNotFoundException
      */
-    private int[][] readMatrix(String filename) throws FileNotFoundException {
+    private static boolean formatCheck(String filename) throws FileNotFoundException, NumberFormatException, InputMismatchException, Exception {
+        boolean valid = false;
+
         File file = new File(filename);
         Scanner scnr = new Scanner(file);
-        int size = scnr.nextInt();
-        this.magicSquareArray = new int[size][size];
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                this.magicSquareArray[i][j] = scnr.nextInt();
+        //May throw NumberFormatException
+        String[] header = scnr.nextLine().split("\\s+");
+        if (header.length != 2) {
+            scnr.close();
+            throw new Exception("Too many arguments in first line");
+        }
+        int rows = Integer.parseInt(header[0]);
+        int columns = Integer.parseInt(header[1]);
+        
+        double[][] matrix = new double[rows][columns];
+        
+        for (int row = 0; row < rows; row++){
+            String line = scnr.nextLine();
+            if (line == "") {
+                scnr.close();
+                throw new Exception("Too few rows");
             }
+            String[] stringArr = line.split("\\s+");
+            int length = stringArr.length;
+            //Checks if correct number of columns in this row
+            if (length != columns) {
+                scnr.close();
+                throw new Exception("Incorrect number of columns");
+            }
+
+            for (int i = 0; i < length; i++) {
+                double numberDouble = Double.parseDouble(stringArr[i]);
+                //System.out.println(numberDouble);
+                matrix[row][i] = numberDouble;
+            }
+            
+        }
+        if (scnr.hasNext()){
+            scnr.close();
+            throw new Exception("Too many rows");
         }
         scnr.close();
-        
-        return this.magicSquareArray;
+
+        valid = true;
+        return valid;
     }
-    //
     
     public static void main(String[] args) {
-        //parse `args` for the inputted filenames
-        //if `args` has 0 length
-            //print usage statement and explanation
-            //exit
-        //try 
-            //open file
-            //parse contents
-            //check if fits criteria (rows/columns and also doubles ONLY)
-        //catch FileNotFoundException
-        //catch NumberFormatException
-        //catch InputMismatchException
-        //finally print Validity
+
+        String usageMessage = "Usage: $ java FormatChecker file1 [file2...fileN]";
+    
+        if (args.length == 0) {
+             System.out.println(usageMessage);
+             return;
+         }
         
-        //for each `arg`
-            //try to open file, if not THROW 
+        for(int i = 0; i < args.length; i++){
+            boolean valid = false;
+            String fileName = args[i];
+            System.out.println(fileName);
+            try {
+                valid = formatCheck(fileName);
+            }catch (FileNotFoundException e){
+                System.out.println(e.toString());
+            }catch (NumberFormatException e) {
+                System.out.println(e.toString());
+            }catch (InputMismatchException e) {
+                System.out.println(e.toString());
+            }catch (Exception e) {
+                System.out.println(e.toString());
+            }finally {
+                if (valid == true) {
+                    System.out.println("VALID");
+                } else {
+                    System.out.println("INVALID");
+                };
+                System.out.println();
+            }
+                //print Validity
+        }
 
 
     }
