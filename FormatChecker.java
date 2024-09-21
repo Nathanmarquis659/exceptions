5fbc9bc1fc90219da045afe80b59ee10a666118a
Nathanmarquis659 <nathanmarquis659@u.boisestate.edu>
2024-09-20 19:44:42 -0600

    Add most of the javadocs for formatCheck

--- FormatChecker.java
+++ FormatChecker.java
@@ -15,23 +15,28 @@ public class FormatChecker {
      3) check if fits criteria (rows/columns and also doubles ONLY)
      * 
      * @param filename is a String used to open a file to read
-     * @return 2d int array for obj read from a file
-     * @throws FileNotFoundException
+     * 
+     * @return returns a boolean of whether or not the file is properly formatted (correct header ,
+     *  # of rows/columns , and data)
+     * 
+     * @throws FileNotFoundException if the file does not exist
+     * @throws NumberFormatException if a number is assigned to the wrong type
+     * @throws Exception if there is a formatting issue such as: Incorrect number of arguments in first line, 
+     * Too few rows, Incorrect number of columns, or Too many rows. Will also catch all other exceptions
      */
-    private static boolean formatCheck(String filename) throws FileNotFoundException, NumberFormatException, InputMismatchException, Exception {
+    private static boolean formatCheck(String filename) throws FileNotFoundException, NumberFormatException, Exception {
         boolean valid = false;
-
         File file = new File(filename);
         Scanner scnr = new Scanner(file);
-        //May throw NumberFormatException
         String[] header = scnr.nextLine().split("\\s+");
+
         if (header.length != 2) {
             scnr.close();
-            throw new Exception("Too many arguments in first line");
+            throw new Exception("Incorrect number of arguments in first line");
         }
+
         int rows = Integer.parseInt(header[0]);
         int columns = Integer.parseInt(header[1]);
-        
         double[][] matrix = new double[rows][columns];
         
         for (int row = 0; row < rows; row++){
@@ -50,17 +55,17 @@ public class FormatChecker {
 
             for (int i = 0; i < length; i++) {
                 double numberDouble = Double.parseDouble(stringArr[i]);
-                //System.out.println(numberDouble);
                 matrix[row][i] = numberDouble;
             }
             
         }
+
         if (scnr.hasNext()){
             scnr.close();
             throw new Exception("Too many rows");
         }
-        scnr.close();
 
+        scnr.close();
         valid = true;
         return valid;
     }
@@ -69,11 +74,14 @@ public class FormatChecker {
 
         String usageMessage = "Usage: $ java FormatChecker file1 [file2...fileN]";
     
+        //Check if there are no arguments, if so print a usage message and return
         if (args.length == 0) {
              System.out.println(usageMessage);
              return;
          }
         
+         //Check each arg file for formatting, and if an exception is caused it will print an explanation
+         //Print whether the file is valid or invalid, for ALL cases
         for(int i = 0; i < args.length; i++){
             boolean valid = false;
             String fileName = args[i];
@@ -96,9 +104,6 @@ public class FormatChecker {
                 };
                 System.out.println();
             }
-                //print Validity
         }
-
-
     }
 }
